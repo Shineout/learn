70313cfe753104855e421f4ab5d2d3a33ad843a9
Oleg Krasnov <mail@olegkrasnov.com>
2014-09-09 14:24:12 +0400

    RESTful контроллеры, часть 1 (эпизод 9)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,32 @@ class ItemsController < ApplicationController
 	def index
 		@items = Item.all
 	end
+
+	# items/1 GET
+	def show
+
+	end
+	# items/new GET
+	def new
+
+	end
+
+	# items/1/edit GET
+	def edit
+
+	end
+
+	# items POST
+	def create
+	end
+
+	# items/1 PUT
+	def update
+
+	end
+
+	# items/1 DELETE
+	def destroy
+
+	end
 end
