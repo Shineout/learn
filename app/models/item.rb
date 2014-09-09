08bba7a5d087aa0266154c72cee21046ee545d98
Oleg Krasnov <mail@olegkrasnov.com>
2014-09-09 13:26:59 +0400

    коллбэки в моделях AR (эпизод 6)

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ActiveRecord::Base
 validates :price, numericality: { greater_than: 0 }
 validates :name, :description, presence: true
+
+after_initialize { puts "initialize" } # Item.new; Item.first
+after_save { puts "save" } # Item.save, Item.update_attributes, Item.create
+after_create { puts "create" } # Item.create
+after_update { puts "update" } # Item.update
+after_destroy { puts "destroy" } # Item.destroy
 end
